63fb5bb61b09b71e0d6ed70dad3e6ecdb382af4c
MrCowFace <59069516+MrCowFace@users.noreply.github.com>
2020-10-13 14:01:22 -0400

    cards

---
+++ Assets/__Scripts/Prospector.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+public class Prospector : MonoBehaviour
+{
+    static public Prospector S;
+
+    [Header("Set in Inspector")]
+    public TextAsset deckXML;
+
+    [Header("Set Dynamically")]
+    public Deck deck;
+
+    private void Awake()
+    {
+        S = this;
+    }
+
+
+    private void Start()
+    {
+        deck = GetComponent<Deck>();
+        deck.InitDeck(deckXML.text);
+        Deck.Shuffle(ref deck.cards);
+
+        Card c;
+        for (int cNum = 0; cNum < deck.cards.Count; cNum++)
+        {
+            c = deck.cards[cNum];
+            c.transform.localPosition = new Vector3((cNum % 13) * 3, cNum / 13 * 4, 0);
+        }
+    }
+
+
+
+}
